7e3b4bdf0cf3ae621b0d16c2725f8fec02680ca5
Renan <ramsp2412@gmail.com>
2021-02-25 19:32:14 -0500

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import { DefaultSeo } from 'next-seo'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
 import SEO from '../config/seo'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
     <>
       <DefaultSeo {...SEO} />
